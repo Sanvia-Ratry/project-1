99fb0bf3bc722b8fc3370caa77591c277b9f55a4
Sanvia ratry <sanvia.reee@gmail.com>
2023-06-24 21:32:54 +0600

    Login successful;

--- src/test/java/com/orangehrmslive/LoginWithTestNG.java
+++ src/test/java/com/orangehrmslive/LoginWithTestNG.java
@@ -1,5 +1,24 @@
 package com.orangehrmslive;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class LoginWithTestNG {
 
+
+    @Test
+    public void LoginShouldSucceed(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.tagName("button")).click();
+}
 }
